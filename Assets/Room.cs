0094d7eaea5b902992625724ead32924de37c7e9
ahmadi18 <ahmadian.pouya@gmail.com>
2020-12-22 13:01:17 -0500

    room prefab.

---
+++ Assets/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public List<GameObject> foods;
+
+    public const float roomDiameter = 28f;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
